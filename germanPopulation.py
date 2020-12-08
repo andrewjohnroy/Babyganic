e70a3d5e2f43923f9c9ab692ec8ea2f59fd1d4b0
Andrew John Roy Stephenson <andrewjohnroy@gmail.com>
2020-12-08 16:33:40 +1100

    Finished.

--- germanPopulation.py
+++ germanPopulation.py
@@ -4,12 +4,22 @@ for line in case:
     arr.append(line.split())
 
 
-def sor(e):
+def sor_growth(e):
     return int(e[8]) - int(e[9])
 
 
-arr.sort(key=sor)
+def sor_max(e):
+    return -int(e[9])
 
-for line in arr:
-    print(line[0] + " " + str(int(line[9]) - int(line[8])) + " population growth from 2010-2015")
 
+arr.sort(key=sor_growth)
+
+for i in range(2):
+    print(arr[i][0] + " is a city for a shop with " + str(int(arr[i][9]) - int(arr[i][8])) + " population growth from "
+                                                                                           "2010-2015")
+print()
+
+arr.sort(key=sor_max)
+
+for i in range(3):
+    print(arr[i][0] + " is a city for sales agents with " + str(int(arr[i][9])) + " population in 2015")
