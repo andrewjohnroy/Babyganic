3ec632f7f0ddf42d627ba313260137014fa0f3fb
Andrew John Roy Stephenson <andrewjohnroy@gmail.com>
2020-12-08 15:33:48 +1100

    more code

--- germanPopulation.py
+++ germanPopulation.py
@@ -1,2 +1,17 @@
 case = open("./case.txt", 'r')
+arr = []
+for line in case:
+    arr.append(line.split())
+
+print(arr)
+
+
+def sor(e):
+    return int(e[9]) - int(e[8])
+
+
+arr.sort(key=sor)
+
+for line in sort:
+    print(line)
 
